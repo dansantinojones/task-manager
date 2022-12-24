e319dfa8a4150207a7e65f5e469c8ab01d6f7077
dansantinojones <dansantinojones@hotmail.co.uk>
2022-12-24 13:40:07 +0000

    Add datepicker and select initialization

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
   // sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+
+  // sidenav initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: 'Select'}
+  });
+
+  // select initialization
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
 });
